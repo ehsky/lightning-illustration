3c4aba07295e704a268da31a0ade25593af5f29b
Erlend <26760784+ehsky@users.noreply.github.com>
2023-03-13 20:27:18 +0100

    :sparkles: Adding a simple lightning illustration component

---
+++ force-app/main/default/lwc/illustration/__tests__/illustration.test.js
@@ -0,0 +1,83 @@
+import { createElement } from "lwc";
+import Illustration from "c/illustration";
+
+const ILLUSTRATIONS = [
+  "Fishing Deals",
+  "Lake Mountain",
+  "No Events",
+  "No Task",
+  "Setup",
+  "No Access",
+  "No Connection",
+  "Not Available In Lightning",
+  "Page Not Available",
+  "Walkthrough Not Available",
+  "Going Camping",
+  "Maintenance",
+  "Gone Fishing",
+  "No Access2",
+  "No Content",
+  "No Preview",
+  "Preview",
+  "Research",
+  "No Data",
+  "Open Road"
+];
+
+describe("c-illustration", () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  it("Render default component behavior with no attributes", () => {
+    const element = createElement("c-illustration", {
+      is: Illustration
+    });
+    document.body.appendChild(element);
+
+    const div = element.shadowRoot.querySelector("div");
+    expect(div.className).toBe("slds-illustration slds-illustration_small");
+
+    const h3 = element.shadowRoot.querySelector("h3");
+    expect(h3.className).toBe("slds-text-heading_medium");
+    expect(h3.textContent).toBe("");
+
+    const p = element.shadowRoot.querySelector("p");
+    expect(p.className).toBe("slds-text-body_regular");
+    expect(p.textContent).toBe("");
+  });
+
+  ILLUSTRATIONS.forEach((illustration) => {
+    it(`Renders "${illustration}" with size "large" with a message header and message body`, () => {
+      const MESSAGE_HEADING = "Test Heading";
+      const MESSAGE_BODY = "Test Body";
+
+      const element = createElement("c-illustration", {
+        is: Illustration
+      });
+      element.illustration = illustration;
+      element.messageHeading = MESSAGE_HEADING;
+      element.messageBody = MESSAGE_BODY;
+      element.size = "large";
+      document.body.appendChild(element);
+
+      const div = element.shadowRoot.querySelector("div");
+      expect(div.className).toBe("slds-illustration slds-illustration_large");
+
+      const svg = element.shadowRoot.querySelector("svg");
+      expect(svg).toBeTruthy();
+      expect(svg.getAttribute("aria-hidden")).toBe("true");
+
+      const h3 = element.shadowRoot.querySelector("h3");
+      expect(h3.className).toBe("slds-text-heading_medium");
+      expect(h3.textContent).toBe(MESSAGE_HEADING);
+
+      const p = element.shadowRoot.querySelector("p");
+      expect(p.className).toBe("slds-text-body_regular");
+      expect(p.textContent).toBe(MESSAGE_BODY);
+    });
+  });
+});
---
+++ force-app/main/default/lwc/illustration/illustration.js
@@ -0,0 +1,107 @@
+import { LightningElement, api } from "lwc";
+/**
+ * Import illustrations
+ * */
+// Custom
+import FISHING_DEALS from "./icons/Custom/fishingDeals.html";
+import LAKE_MOUNTAIN from "./icons/Custom/lakeMountain.html";
+import NO_EVENTS from "./icons/Custom/noEvents.html";
+import NO_TASK from "./icons/Custom/noTask.html";
+import SETUP from "./icons/Custom/setup.html";
+// Error
+import NO_ACCESS from "./icons/Error/noAccess.html";
+import NO_CONNECTION from "./icons/Error/noConnection.html";
+import NOT_AVAILABLE_IN_LIGHTNING from "./icons/Error/notAvailableInLightning.html";
+import PAGE_NOT_AVAILABLE from "./icons/Error/pageNotAvailable.html";
+import WALKTHROUGH_NOT_AVAILABLE from "./icons/Error/walkthroughNotAvailable.html";
+// Informational
+import GOING_CAMPING from "./icons/Informational/goingCamping.html";
+import MAINTENANCE from "./icons/Informational/maintenance.html";
+// Miscellaneous
+import GONE_FISHING from "./icons/Miscellaneous/goneFishing.html";
+import NO_ACCESS2 from "./icons/Miscellaneous/noAccess2.html";
+import NO_CONTENT from "./icons/Miscellaneous/noContent.html";
+import NO_PREVIEW from "./icons/Miscellaneous/noPreview.html";
+import PREVIEW from "./icons/Miscellaneous/preview.html";
+import RESEARCH from "./icons/Miscellaneous/research.html";
+// No Data
+import NO_DATA from "./icons/NoData/dessert.html";
+import OPEN_ROAD from "./icons/NoData/openRoad.html";
+
+export default class Illustration extends LightningElement {
+  /**
+   * The size of the illustration to render.
+   * Possible values are `small` or `large`.
+   * @type {string}
+   * @access public
+   */
+  @api size;
+
+  @api messageHeading;
+  @api messageBody;
+  @api illustration = "";
+
+  get includesMessage() {
+    return !!this.messageHeading || !!this.messageBody;
+  }
+
+  get illustrationStyle() {
+    let classes = "slds-illustration";
+    return this.size === "large"
+      ? (classes += " slds-illustration_large")
+      : (classes += " slds-illustration_small");
+  }
+
+  render() {
+    const illustration = this.illustration.split(" ").join("_").toUpperCase();
+    switch (illustration) {
+      // Custom
+      case "FISHING_DEALS":
+        return FISHING_DEALS;
+      case "LAKE_MOUNTAIN":
+        return LAKE_MOUNTAIN;
+      case "NO_EVENTS":
+        return NO_EVENTS;
+      case "NO_TASK":
+        return NO_TASK;
+      case "SETUP":
+        return SETUP;
+      // Error
+      case "NO_ACCESS":
+        return NO_ACCESS;
+      case "NO_CONNECTION":
+        return NO_CONNECTION;
+      case "NOT_AVAILABLE_IN_LIGHTNING":
+        return NOT_AVAILABLE_IN_LIGHTNING;
+      case "PAGE_NOT_AVAILABLE":
+        return PAGE_NOT_AVAILABLE;
+      case "WALKTHROUGH_NOT_AVAILABLE":
+        return WALKTHROUGH_NOT_AVAILABLE;
+      // Informational
+      case "GOING_CAMPING":
+        return GOING_CAMPING;
+      case "MAINTENANCE":
+        return MAINTENANCE;
+      // Miscellaneous
+      case "GONE_FISHING":
+        return GONE_FISHING;
+      case "NO_ACCESS2":
+        return NO_ACCESS2;
+      case "NO_CONTENT":
+        return NO_CONTENT;
+      case "NO_PREVIEW":
+        return NO_PREVIEW;
+      case "PREVIEW":
+        return PREVIEW;
+      case "RESEARCH":
+        return RESEARCH;
+      // No Data
+      case "NO_DATA":
+        return NO_DATA;
+      case "OPEN_ROAD":
+        return OPEN_ROAD;
+      default:
+        return FISHING_DEALS;
+    }
+  }
+}
